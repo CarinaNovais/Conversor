606f96206e84565477a56e0bf6af29635f953235
Carina Novais <80132456+CarinaNovais@users.noreply.github.com>
2023-03-08 10:40:23 -0300

    adc conversoes de mais moedas

--- src/Moeda.java
+++ src/Moeda.java
@@ -9,12 +9,24 @@ public class Moeda {
         this.valor = valor;
     }
 
-    public float ConverteReaisDolar(){
-        return (float) (this.valor/5.17);
+    public float ConverteDolarReais() {
+        return (float) (this.valor * 5.17);
     }
 
-    public float ConverteDolarReais(){
-        return (float) (this.valor * 5.17);
+    public float ConverteEuroReais() {
+        return (float) (this.valor * 0.18);
+    }
+
+    public float ConverteLibrasEsterlinasReais() {
+        return (float) (this.valor * 0.16);
+    }
+
+    public float ConvertePesoArgentinoReais() {
+        return (float) (this.valor * 38.75);
+    }
+
+    public float ConvertePesoChilenoReais() {
+        return (float) (this.valor * 155.07);
     }
 
 
--- src/Temperatura.java
+++ src/Temperatura.java
@@ -1,8 +1,8 @@
 public class Temperatura {
     private float cels;
 
-    public float RetornaF(){
-        return (float) (this.cels * 9/5) + 32;
+    public float RetornaF() {
+        return (float) (this.cels * 9 / 5) + 32;
     }
 
     public float getCels() {
